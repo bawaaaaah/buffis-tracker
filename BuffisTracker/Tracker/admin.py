2cc1d1bd9eda75a7983d8447fb3b2e514db5f1d8
bjorn.kempen <bjorn.kempen@807707f6-f85e-11de-8591-91bc733d3be8>
2010-01-03 17:16:36 +0000

    Initial commit

---
+++ BuffisTracker/Tracker/admin.py
@@ -0,0 +1,10 @@
+from BuffisTracker.Tracker.models import *
+from django.contrib import admin
+
+admin.site.register(Torrent)
+admin.site.register(UserProfile)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Peer)
+
